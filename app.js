adb78a0b5b85b8ec69bca15e707041b47f9d7a2d
bfredl <bjorn.linse@gmail.com>
2023-07-14 15:24:15 +0200

    some annotate

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 let midi = null;
+/** @type {MIDIInput} */
 let delugeIn = null;
+/** @type {MIDIOutput} */
 let delugeOut = null;
 let theInterval = null;
 
@@ -77,19 +79,20 @@ function getOled() {
 
 function setRefresh() {
   if (theInterval != null) {
-    window.clearInterval(theInterval)
+    clearInterval(theInterval)
     theInterval = null;
   }
 
   let value = parseInt($("msInput").value);
 
   if (value > 0) {
-    theInterval = window.setInterval(getOled, value);
+    theInterval = setInterval(getOled, value);
   }
 }
 
 let lastmsg
 
+/** @param {MIDIMessageEvent} msg */
 function handleData(msg) {
   lastmsg = msg
   console.log(msg.data);
@@ -100,6 +103,7 @@ function handleData(msg) {
   decode(msg.data)
 }
 
+/** @param {Uint8Array} data */
 function decode(data) {
   if (data.length < 3 || data[0] != 0xf0 || data[1] != 0x7d) {
     console.log("foreign sysex?")
@@ -120,6 +124,7 @@ function decode(data) {
     let unpacked = unpack(packed)
     console.log("unpacked size "+ unpacked.length);
 
+    /** @type {CanvasRenderingContext2D} */
     let ctx = $("myCanvas").getContext("2d")
 
     let px_height = 5;
@@ -155,6 +160,7 @@ function decode(data) {
   }
 }
 
+/** @param {Uint8Array} src */
 function unpack(src) {
   let src_len = src.length
   let dst = new Uint8Array(2048); // TODO: dynamic size!
@@ -162,6 +168,7 @@ function unpack(src) {
 
   let d = 0;
   let s = 0;
+
 
   while (s+1 < src_len) {
     let first = src[s++];
@@ -208,9 +215,8 @@ function unpack(src) {
   return dst.subarray(0,d);
 }
 
-testdata = new Uint8Array ([
+testdata = new Uint8Array([
     240, 125, 2, 64, 1, 0, 126, 127, 0, 102, 0, 66, 76, 71, 18, 44, 100, 0, 6, 8, 112, 36, 8, 6, 0, 126, 8, 16, 16, 32, 126, 0, 68, 2, 67, 126, 68, 2, 2, 0, 126, 70, 16, 67, 126, 126, 127, 0, 114, 0, 71, 64, 72, 0, 69, 124, 68, 108, 3, 124, 120, 68, 0, 67, 96, 69, 120, 70, 12, 69, 120, 57, 96, 0, 0, 112, 124, 27, 28, 124, 112, 0, 68, 0, 69, 124, 69, 76, 5, 124, 120, 48, 68, 0, 69, 124, 68, 12, 10, 28, 120, 112,
     68, 0, 20, 48, 120, 124, 108, 69, 76, 67, 0, 84, 0, 67, 96, 69, 120, 70, 12, 69, 120, 67, 96, 68, 0, 69, 124, 71, 76, 66, 12, 86, 0, 69, 124, 68, 12, 10, 28, 120, 112, 70, 0, 69, 124, 70, 108, 66, 12, 70, 0, 69, 124, 98, 0, 68, 7, 68, 6, 0, 7, 3, 70, 0, 68, 3, 70, 6, 68, 3, 12, 0, 4, 7, 3, 70, 1, 12, 3, 7, 4, 0, 68, 7, 28, 0, 1, 7, 6, 4, 68, 0, 68, 7, 68, 6, 4, 7, 3, 1,
     68, 0, 66, 2, 70, 6, 4, 7, 3, 1, 86, 0, 68, 3, 70, 6, 68, 3, 70, 0, 68, 7, 94, 0, 68, 7, 68, 6, 4, 7, 3, 1, 70, 0, 68, 7, 72, 6, 70, 0, 68, 7, 72, 6, 126, 98, 0, 247
 ]);
-
