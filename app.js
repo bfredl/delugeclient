40733ecebc5d4ef4f85c5a2beae57b62bed3816e
bfredl <bjorn.linse@gmail.com>
2023-07-15 13:15:03 +0200

    having fun with wasm

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import {loadlib} from "./lib.js";
+
 /** @type {MIDIAccess} */
 let midi = null;
 /** @type {MIDIInput} */
@@ -6,8 +8,12 @@ let delugeIn = null;
 let delugeOut = null;
 let theInterval = null;
 
-let devicesIn = {};
-let devicesOut = {};
+let lib = null;
+loadlib(function(l) {
+  lib = l;
+  window.lib = l;
+})
+
 
 function $(name) {
   return document.getElementById(name)
@@ -141,6 +147,7 @@ window.addEventListener('load', function() {
   return;
 });
 
+
 function pingTest() {
     delugeOut.send([0xf0, 0x7d, 0x00, 0xf7]);
 }
@@ -213,7 +220,7 @@ function decode(data) {
     let packed = data.subarray(6,data.length-1)
     console.log("packed size "+ packed.length);
 
-    let unpacked = unpack(packed)
+    let unpacked = lib.wrap_array(lib.fn.unpack_7to8_rle, packed)
     console.log("unpacked size "+ unpacked.length);
 
     /** @type {CanvasRenderingContext2D} */
@@ -252,62 +259,7 @@ function decode(data) {
   }
 }
 
-/** @param {Uint8Array} src */
-function unpack(src) {
-  let src_len = src.length
-  let dst = new Uint8Array(2048); // TODO: dynamic size!
-  let dst_size = 2048;
-
-  let d = 0;
-  let s = 0;
-
-
-  while (s+1 < src_len) {
-    let first = src[s++];
-    if (first < 64) {
-      let size = 0; let off = 0;
-      if (first < 4) { size = 2; off = 0; }
-      else if (first < 12) { size = 3; off = 4; }
-      else if (first < 28) { size = 4; off = 12; }
-      else if (first < 60) { size = 5; off = 28; }
-      else {
-        return -7;
-      }
-
-      if (size > src_len-s) {
-        // printf("s: %d, d: %d, first: %d\n", s, d, first);
-        return -1;
-      }
-      if (size > dst_size-d) return -11;
-      let highbits = first-off;
-      for (let j = 0; j < size; j++) {
-        dst[d+j] = src[s+j] & 0x7f;
-        if (highbits & (1<<j)) {
-          dst[d+j] |= 0x80;
-        }
-      }
-
-      d += size;
-      s += size;
-    } else {
-      // first = 64 + (runlen<<1) + highbit
-      first = first-64;
-      let high = (first&1);
-      let runlen = first >> 1;
-      if (runlen == 31) {
-        runlen = 31 + src[s++];
-        if (s == src_len) return -3;
-      }
-      let byteval = src[s++] + 128*high;
-      if (runlen > dst_size-d) return -12;
-      dst.fill(byteval, d, d+runlen);
-      d += runlen;
-    }
-  }
-  return dst.subarray(0,d);
-}
-
-testdata = new Uint8Array([
+let testdata = new Uint8Array([
     240, 125, 2, 64, 1, 0, 126, 127, 0, 102, 0, 66, 76, 71, 18, 44, 100, 0, 6, 8, 112, 36, 8, 6, 0, 126, 8, 16, 16, 32, 126, 0, 68, 2, 67, 126, 68, 2, 2, 0, 126, 70, 16, 67, 126, 126, 127, 0, 114, 0, 71, 64, 72, 0, 69, 124, 68, 108, 3, 124, 120, 68, 0, 67, 96, 69, 120, 70, 12, 69, 120, 57, 96, 0, 0, 112, 124, 27, 28, 124, 112, 0, 68, 0, 69, 124, 69, 76, 5, 124, 120, 48, 68, 0, 69, 124, 68, 12, 10, 28, 120, 112,
     68, 0, 20, 48, 120, 124, 108, 69, 76, 67, 0, 84, 0, 67, 96, 69, 120, 70, 12, 69, 120, 67, 96, 68, 0, 69, 124, 71, 76, 66, 12, 86, 0, 69, 124, 68, 12, 10, 28, 120, 112, 70, 0, 69, 124, 70, 108, 66, 12, 70, 0, 69, 124, 98, 0, 68, 7, 68, 6, 0, 7, 3, 70, 0, 68, 3, 70, 6, 68, 3, 12, 0, 4, 7, 3, 70, 1, 12, 3, 7, 4, 0, 68, 7, 28, 0, 1, 7, 6, 4, 68, 0, 68, 7, 68, 6, 4, 7, 3, 1,
     68, 0, 66, 2, 70, 6, 4, 7, 3, 1, 86, 0, 68, 3, 70, 6, 68, 3, 70, 0, 68, 7, 94, 0, 68, 7, 68, 6, 4, 7, 3, 1, 70, 0, 68, 7, 72, 6, 70, 0, 68, 7, 72, 6, 126, 98, 0, 247
